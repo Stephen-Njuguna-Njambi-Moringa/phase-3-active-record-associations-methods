dcabf199503498d043c3535ae3e1cebe7d304828
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-11-07 23:36:07 +0300

    updates in progress

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,8 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    #return the genre of the artist's first saved song
+
   end
 
   def song_count
